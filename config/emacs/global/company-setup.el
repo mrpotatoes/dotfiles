4fef5fcd31369d6aa57e33e454de31d65a271d1c
mrpotatoes <contact@andricvillanueva.com>
2014-04-08 09:39:23 -0400

    Added a bunch of functionality. Finally fixed solarized by using a better repo.

---
+++ config/emacs/global/company-setup.el
@@ -0,0 +1,20 @@
+
+;; --------------------------------------------------------------------------------
+;; Company mode
+;;  - https://github.com/company-mode/company-mode
+;;  - http://company-mode.github.io/
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(setq company-backends 
+  '(company-elisp 
+    company-ropemacs
+    company-etags
+    company-dabbrev-code
+    company-keywords
+    company-files 
+    company-dabbrev))
+
+;; Keyboard Bindings.
+(global-set-key (kbd "M-r") 'company-complete-common)
+
