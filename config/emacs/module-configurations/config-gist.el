fb38cd7e8d4589807134373567f0c4ffadac52e2
Andric Villanueva <contact@andricvillanueva.com>
2014-05-06 21:36:35 -0400

    horray

---
+++ config/emacs/module-configurations/config-gist.el
@@ -0,0 +1,4 @@
+;; -----------------------------------------------------------------------------
+;; My gists setup.
+(define-key my-keys-minor-mode-map (kbd "C-c C-g") 'gist-region)
+(autoload 'gist-region "gist")
