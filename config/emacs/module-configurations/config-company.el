931e2ac6192d7a3f03d379deea0f6f1deae57d9e
Andric Villanueva <contact@andricvillanueva.com>
2014-05-06 20:28:52 -0400

    Apparently this was needed.

---
+++ config/emacs/module-configurations/config-company.el
@@ -0,0 +1,33 @@
+;; --------------------------------------------------------------------------------
+;; Company mode
+;;  - https://github.com/company-mode/company-mode
+;;  - http://company-mode.github.io/
+;;  - https://github.com/zmyrgel/zf-mode
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(setq company-backends
+  '(company-elisp
+    company-ropemacs
+    company-etags
+    company-dabbrev-code
+    company-keywords
+    company-files
+    company-dabbrev
+))
+
+; Keyboard Bindings.
+(global-set-key (kbd "M-r") 'company-complete-common)
+
+(setq company-idle-delay 0.3)
+(setq company-tooltip-limit 20)
+(setq company-minimum-prefix-length 2)
+(setq company-echo-delay 0)
+(setq company-auto-complete nil)
+
+(global-company-mode 1)
+
+;; (add-to-list 'company-backends 'company-dabbrev t)
+;; (add-to-list 'company-backends 'company-ispell t)
+;; (add-to-list 'company-backends 'company-files t)
+;; (setq company-backends (remove 'company-ropemacs company-backends))
